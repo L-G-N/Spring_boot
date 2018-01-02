29a21c836158a477c7a4b08df46066aa5978d345
L-G-N <logulgn@live.co.uk>
2018-01-02 22:34:04 +0530

    added uniqz project

---
+++ uniqz/src/main/java/com/uniqz/UniqzApplication.java
@@ -0,0 +1,12 @@
+package com.uniqz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqzApplication.class, args);
+	}
+}
